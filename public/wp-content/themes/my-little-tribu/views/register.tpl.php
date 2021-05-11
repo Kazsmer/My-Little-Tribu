bc34f2aa02b0fbd329eb46b995c7581c401f946e
jeremie <jeremie.bauder.jb@gmail.com>
2021-05-11 16:43:53 +0200

    création du register.tpl.php

--- public/wp-content/themes/my-little-tribu/views/register.tpl.php
+++ public/wp-content/themes/my-little-tribu/views/register.tpl.php
@@ -11,11 +11,22 @@ get_header();
 <body>
   <?php get_template_part('partials/header.tpl');?>
   <main id="main">
+    <?php
+      if(array_key_exists('error', $args)) {
+          echo '<div class="class error">';
+              echo '<ul>';
+              foreach($args['messages'] as $message) {
+                  echo '<li>' . $message . '</li>';
+              }
+              echo '</ul>';
+          echo '</div>';
+      }
+    ?>
     <div class="site-section">
 
       <div class="site-section pb-0">
         <div class="container">
-          <form class="row" data-aos="fade-up">
+          <form method="post" action="<?=get_home_url();?>/user/create" class="row" data-aos="fade-up" >
             <div class="col-lg-12">
               <div class="flex px-0 pb-4">              
                 <div class="col-lg-6 pl-0">
@@ -45,8 +56,6 @@ get_header();
                 <button class="col-md-12 col-sm-9 col-sm-12 col-12 readmore">
                   Créer mon compte
                 </button>
-              </div>
-              <div class="col-auto px-0 pt-4">
               </div>
             </div>
             </form>
