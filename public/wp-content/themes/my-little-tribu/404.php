66cbe14d47dbfc10824c91b8d98b48683927fea4
Fanni Kazsmer <kazsmer.fanni@gmail.com>
2021-05-26 10:28:49 +0200

    test modif aws

--- public/wp-content/themes/my-little-tribu/404.php
+++ public/wp-content/themes/my-little-tribu/404.php
@@ -17,7 +17,7 @@ get_header();
       <div class="container h-100 d-flex align-items-center ">
         <div class="row mb-5 w-100">
           <div class="col-md-12 col-lg-6 mb-4 mb-lg-0" data-aos="fade-up">
-            <h2 style="font-size:10rem;font-weight:bold;">404</h2>
+            <h2 style="font-size:10rem;font-weight:bold;">404 Page non trouvée</h2>
           </div>
         </div>
       </div>
